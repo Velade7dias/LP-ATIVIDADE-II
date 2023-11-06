5037ebf93aa0d393f4507d096189e0650bbad897
Nicolas Brito <143547528+Velade7dias@users.noreply.github.com>
2023-11-06 16:31:17 -0300

    Exercício9.c

--- Exercício9.c
+++ Exercício9.c
@@ -3,25 +3,45 @@
 #include <string.h>
 #include <locale.h>
 
-int main () {
+int main()
+{
 
+    setlocale(LC_ALL, "portuguese");
 
+    int numero;
+    int i;
+    int primos = 1;
 
+    printf("Digite um número:");
+    scanf("%d", &numero);
 
+    if (numero <= 1)
+    {
+        primos = 0;
+    }
+    else
+    {
+        for (i = 2; i <= numero / 2; i++)
+        {
+            if (numero % i == 0)
+            {
+                primos = 0;
+                break;
+            }
+        }
+    }
 
+    switch (primos)
+    {
+    case 1:
+        printf("Número primo\n");
+        break;
+    default:
+        printf("Número não primo\n");
+        break;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    getchar();
+    
     return 0;
 }
